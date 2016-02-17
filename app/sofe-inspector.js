302fb5f8ec0e1a9cda5dffe93f3c92f88f993ab0
Bret Little <bret.little@gmail.com>
2016-02-16 19:02:12 -0700

    Initial commmmmmit

---
+++ app/sofe-inspector.js
@@ -0,0 +1,6 @@
+chrome.devtools.panels.create(
+	"SofeInspector",
+	"badge.png",
+	"sofe-inspector.html",
+	function cb(panel) {}
+);
---
+++ app/src/main.js
@@ -0,0 +1,9 @@
+import React from 'react/dist/react.min.js';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<div class='row'>
+	<div class='col-xs-12'>
+		<h1>sofe inspector</h1>
+	</div>
+</div>, document.getElementById('app'));
+			
