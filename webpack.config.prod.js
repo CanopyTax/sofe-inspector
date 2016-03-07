27ebf47d6b350054211016bcbbdeaf52a1f56c1b
Bret Little <bret.little@gmail.com>
2016-03-07 10:39:09 -0700

    Add prod build

---
+++ webpack.config.prod.js
@@ -0,0 +1,26 @@
+var webpack = require('webpack');
+var path = require("path");
+
+module.exports = {
+    entry: {
+			main: "./src/main.js",
+    },
+    output: {
+				path: path.join(__dirname, "dist"),
+        filename: "[name].js"
+    },
+		module: {
+			loaders: [
+				{ test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"}
+			]
+		},
+		devtool: 'sourcemap',
+		plugins: [
+			new webpack.optimize.UglifyJsPlugin(),
+			new webpack.DefinePlugin({
+				"process.env": {
+					NODE_ENV: JSON.stringify('production')
+				}
+			})
+		]
+}
