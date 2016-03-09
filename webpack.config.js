61dc96370717dfe30e844b03a34f5b50605fa1ae
Bret Little <bret.little@gmail.com>
2016-03-08 17:49:34 -0700

    Change surge path

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var path = require("path");
+var version = require('./package.json').version;
 
 module.exports = {
     entry: {
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,5 +1,6 @@
 var webpack = require('webpack');
 var path = require("path");
+var version = require('./package.json').version;
 
 module.exports = {
     entry: {
@@ -7,11 +8,16 @@ module.exports = {
     },
     output: {
 				path: path.join(__dirname, "dist"),
-        filename: "sofe-inspector.js"
+        filename: "sofe-inspector-" + version + ".js"
     },
 		module: {
 			loaders: [
-				{ test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"}
+				{ test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"},
+				{ test: /\.css$/, loaders: [
+					'style-loader',
+					'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]',
+					'postcss-loader'
+				]}
 			]
 		},
 		devtool: 'sourcemap',
